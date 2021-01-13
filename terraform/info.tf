dae6023e02d3634138edd01c906ef5177bccb167
Gus Silva <gsilva.1108@gmail.com>
2021-01-13 11:57:32 -0500

    updated info.tf

--- terraform/info.tf
+++ terraform/info.tf
@@ -1,6 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    shared_credentials_file = "/Users/gsilva/.aws/credentials"
+    shared_credentials_file = "/home/gus/.aws/credentials"
+    profile = "pi_user"
 }
 
 variable "user_name" {
