5796420875dc4e910427dcb848d64a3f7a3b6eb9
gsilva1108 <52945302+gsilva1108@users.noreply.github.com>
2021-01-13 12:05:42 -0500

    Updated info.tf

--- terraform/info.tf
+++ terraform/info.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIA2GAAIISJXKXIH4PF"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 variable "user_name" {
